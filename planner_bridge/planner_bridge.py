3e1428aebf93880dce338bf88c1d955950c9db32
johntgz <johntan@openrobotics.org>
2023-03-03 02:04:15 +0800

    updated to check and remove inactive agents

--- planner_bridge/planner_bridge.py
+++ planner_bridge/planner_bridge.py
@@ -29,7 +29,19 @@ def get_euclid_dist(point_a, point_b):
     return np.sqrt(dx * dx + dy * dy + dz * dz)
 
 class Agent:
-    def __init__(self, agent_id):
+    def __init__(self, agent_id=""):
+        """_summary_
+
+        Parameters
+        ----------
+        agent_id : String
+            ID of agent
+
+        Raises
+        ------
+        Exception
+            Exception raised when empty agent_id is given to constructor
+        """
         if agent_id == "":
             raise Exception("Created an Agent object without an ID!")
         
@@ -37,9 +49,12 @@ class Agent:
         self.executing_waypoint = False  # True if agent is in the process of executing waypoint else False.
         self.current_waypoint_cmd = None  # Current waypoint being executed (of type UserCommand)
         self.flight_state = None  # Current state of agent
+        self.connected = None  
+        self.completed = None  
+        self.mission_capable = None  
         self.actual_pos = None  # Current actual position
         self.waypoint_queue = [] # List of waypoints to fulfill
-        self.time_at_last_cmd = time()  # Time elapsed since last waypoint issued
+        self.time_since_pose_update = time()  # Time elapsed since robot pose was last updated
 
     def add_waypoints(self, waypoints):
         """Add a list of waypoints to the agent
@@ -52,12 +67,23 @@ class Agent:
         self.waypoint_queue.extend(waypoints)
 
     def get_dist_to_goal(self) -> float :
+        """Get distance from agent's current position to goal
+
+        Returns
+        -------
+        float
+            Distance to goal in meters
+        """
         return get_euclid_dist(self.actual_pos.pose.position, self.current_waypoint_cmd.goal)
 
-    def is_waypoint_queue_empty(self) -> bool:
-        return len(self.waypoint_queue) == 0
-
     def new_waypoint_cmd(self, waypoint_cmd):
+        """Set Agent to new waypoint command
+
+        Parameters
+        ----------
+        waypoint_cmd : UserCommand
+            Waypoint command issued to agent
+        """
         self.executing_waypoint = True
         self.current_waypoint_cmd = waypoint_cmd
 
@@ -115,6 +141,8 @@ class planner_ROS(Node):
             "cf2": Agent("cf2"),
             "cf3": Agent("cf3"),
         }
+
+        self.inactive_agent_list = []
 
         # Add waypoints to each agent
         for item in self.agent_list.items():
@@ -188,18 +216,21 @@ class planner_ROS(Node):
         """
         if agent_id in self.agent_list:
             self.agent_list[agent_id].actual_pos = pose
+            self.agent_list[agent_id].time_since_pose_update = time()
 
     def agent_state_callback(self, agent_states):
-        time_now = time()
 
         for agent in agent_states.agents:
-            if agent.id not in self.agent_list:
-                # self.get_logger().error(
-                #     f"Agent {agent.id} has been removed from agent list. Ignoring..."
-                # )
-                continue
-
-            self.agent_list[agent.id].flight_state = agent.flight_state
+            if agent.id in self.agent_list:
+                self.agent_list[agent.id].flight_state = agent.flight_state
+                self.agent_list[agent.id].connected = agent.connected  
+                self.agent_list[agent.id].completed = agent.completed  
+                self.agent_list[agent.id].mission_capable = agent.mission_capable  
+            # IF receiving pose of inactive agent, ignore
+            else:
+                self.get_logger().error(
+                    f"Agent {agent.id} has been removed from agent list. Ignoring..."
+                )
 
     def publish_waypoints(self):
 
@@ -215,7 +246,7 @@ class planner_ROS(Node):
                 self.usercommand_pub.publish(agent.current_waypoint_cmd)
 
             # IF waypoints queue is not empty, send a new waypoint from the existing waypoint_queue
-            elif not agent.is_waypoint_queue_empty():
+            elif len(agent.waypoint_queue) != 0:
                 new_wp = agent.waypoint_queue[0]
 
                 waypoint_cmd = self.create_usercommand(
@@ -237,9 +268,6 @@ class planner_ROS(Node):
                 # Publish waypoint command to the agent
                 self.usercommand_pub.publish(waypoint_cmd)
 
-                # Refresh time since last waypoint was published
-                agent.time_at_last_cmd = time()
-
                 self.get_logger().info(
                     "Published new waypoint ({},{},{}) with yaw {} for agent {}".format(
                         waypoint_cmd.goal.x,
@@ -251,17 +279,43 @@ class planner_ROS(Node):
                 )
 
     def check_agent_timer_callback(self):
-        """Check if agents has fulfilled waypoint. IF so, prepare it for the next waypoint 
+        """Check if agents: 
+        1. Are in active flight state (not landing state). If False, remove from active agent list
+        2. have time exceeded since updating last pose state (not landing state). If False, remove from active agent list
+        3. Have an actual pose?  If not, send warning
+        4. have fulfilled the goal, if so then activate flags for next waypoint.
         """
+        time_now = time()
         for item in self.agent_list.items():
             agent_id = item[0]
             agent = item[1]
 
+            # Check 1: If agent is active. If not remove from list 
+            # and add to inactive list
+            if agent.flight_state not in {
+                AgentState.MOVE,
+                AgentState.IDLE,
+                AgentState.TAKEOFF,
+                AgentState.HOVER,
+            }:
+                self.get_logger().warn(f"'{agent_id}' is in {agent.flight_state} State, and is classified as INACTIVE")
+                # Remove from Active agent list
+                self.remove_inactive_agent(agent_id)
+                continue
+            
+            # Check 2: Timeout exceeded since last pose received?
+            if abs(time_now - agent.time_since_pose_update) > self.agent_timeout:
+                self.get_logger().warn(f"Timeout exceeded in updating pose for Agent '{agent_id}'")
+                # Remove from list
+                self.remove_inactive_agent(agent_id)
+                continue
+
+            # Check 3: Does agent have an acual position? If not, give a warning message
             if agent.actual_pos is None:
                 self.get_logger().warn(f"No actual position obtained for Agent '{agent_id}'")
                 continue
 
-            # Check if agent has fulfilled goal
+            # Check 4: if agent has fulfilled goal
             # If so, set "executing_waypoint" to FALSE so that agent is ready to accept new waypoints
             if agent.executing_waypoint and agent.current_waypoint_cmd:
 
@@ -273,16 +327,32 @@ class planner_ROS(Node):
                     self.get_logger().info(f"Agent {agent_id} has fulfilled waypoint, ready to accept next waypoint")
                     agent.complete_waypoint_cmd()
 
-            # self.get_logger().info(
-            #     f"""{agent_id} \n
-            #     flight_state: {agent['flight_state']}
-            #     actual_pos: {agent['actual_pos']}
-            #     time_at_last_cmd: {agent['time_at_last_cmd']}
-            #     connected: {agent['connected']}
-            #     completed: {agent['completed']}
-            #     mission_capable: {agent['mission_capable']}
-            #     """
-            # )
+            self.get_logger().info(
+                f"""{agent_id} \n
+                flight_state: {agent.flight_state}
+                actual_pos: {agent.actual_pos}
+                time_since_pose_update: {agent.time_since_pose_update}
+                connected: {agent.connected}
+                completed: {agent.completed}
+                mission_capable: {agent.mission_capable}
+                """
+            )
+
+    def remove_inactive_agent(self, agent_id):
+        """Remove inactive agent from active agent list and add to inactive agent list
+
+        Parameters
+        ----------
+        agent_id : String
+            Agent ID
+        """
+        try:
+            self.agent_list.pop(agent_id)
+            self.inactive_agent_list.append(agent_id)
+            self.get_logger().warn(f"Removed inactive agent '{agent_id}'")
+
+        except ValueError:
+            self.get_logger().error(f"Unable to pop '{agent_id}' out of active agent list")
 
     def takeoff_all(self):
         """Helper method to command all crazyflies to take off
